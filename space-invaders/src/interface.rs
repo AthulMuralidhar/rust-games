8568a11ca7c388cb3032dc45fe28e8b8106fdbaf
Athul Muralidhar <athul.muralidhar@bricr.com>
2021-11-18 16:34:17 +0100

    interfaces.rs wip

--- space-invaders/src/interface.rs
+++ space-invaders/src/interface.rs
@@ -1,36 +1,67 @@
 // https://github.com/bgaster/rusty-space-invaders/blob/master/src/interface_desktop.in
 
-use winit::window::Window;
+use winit::window::{Window, WindowBuilder};
 use winit_input_helper::WinitInputHelper;
-use pixels::Pixels; 
-use winit::event::Event;
+use pixels::{Pixels, SurfaceTexture, wgpu::Surface}; 
+use winit::event::{Event, VirtualKeyCode};
 use winit::event_loop::EventLoop;
+use winit::dpi::LogicalSize;
 
-pub struct Inteface {
+const WIDTH :f64 = 480.0;
+const HEIGHT :f64 = 460.0;
+
+pub struct Interface {
     window: Window,
     input: WinitInputHelper, 
-    pixels: Pixels
+    pixels: Pixels,
+    hidpi_factor: f64
 }
 
-impl Inteface {
+impl Interface {
     pub fn request_redraw(&self){
         self.window.request_redraw();
     }
 
-    pub fn handle_input(&mut self, event: Event){
+    pub fn handle_input(&mut self, event: Event<()>){
         let controls = {
             let mut left = self.input.key_held(VirtualKeyCode::Left);
             let mut right = self.input.key_held(VirtualKeyCode::Right);
             let mut fire = self.input.key_held(VirtualKeyCode::Space);
-        }
+        };
 
         let direction = {
 
-        }
+        };
     }
 
+}
 
-
-    pub fn create_interface(){
+pub fn create_interface() -> (EventLoop<()>, Interface) {
         let event_loop = EventLoop::new();
+        let window_size = LogicalSize::new(WIDTH, HEIGHT);
+        let window = WindowBuilder::new()
+            .with_title("Space Invadorz")
+            .with_inner_size(window_size)
+            .build(&event_loop)
+            .unwrap();
+        
+        let hidpi_factor = window.scale_factor();
+        let surface = Surface::create(&window);
+
+        // let pixels = {
+
+        // }
+
+        (event_loop, Interface {
+            window,
+            hidpi_factor,
+            input: WinitInputHelper::new(),
+            pixels: Pixels::new(
+                width: WIDTH, 
+                height: HEIGHT, 
+                surface: Surface::create(&window)
+                surface_texture: SurfaceTexture::new(WIDTH as u32, HEIGHT as u32, surface)
+            ).unwrap()
+        });
+    
     }
--- space-invaders/src/main.rs
+++ space-invaders/src/main.rs
@@ -1,5 +1,9 @@
 // original sauce: https://github.com/bgaster/rusty-space-invaders/tree/master/src
 
+mod interface;
+
+
+use interface::create_interface;
 
 fn main() {
     env_logger::init();
